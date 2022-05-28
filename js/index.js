cb5508c4b8e66d43b3e7422b4f7270d4a41f1806
Seb <emery.sebastien@hotmail.fr>
2022-05-28 12:00:43 +0200

    Création des composants, mise en place sass, début du style

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const app = {
     state: {
+        notes: [1, 2, 3, 4, 5],
         noteSelected: null,
     },
     init: function() {
@@ -15,18 +16,33 @@ const app = {
             className: 'rating-box',
         });
 
-        const icon = app.configureElement('div', ratingBox, {
+        const icon = app.configureElement('img', ratingBox, {
             className: 'rating-box-icon',
-            textContent: 'STARS',
+            src: 'images/icon-star.svg',
         });
         const title = app.configureElement('h2', ratingBox, {
             className: 'rating-box-title',
             textContent: 'How did we do?'
-        })
+        });
         const text = app.configureElement('p', ratingBox, {
             className: 'rating-box-text',
             textContent: 'Please let us know how we did with your support request. All feedback is appreciated to help us improve our offering!'
         });
+        const rating = app.configureElement('div', ratingBox, {
+            className: 'rating-box-rating',
+        });
+        app.state.notes.forEach(number => {
+            const note = app.configureElement('div', rating, {
+                className: 'rating-box-rating-note',
+                textContent: number,
+                value: number,
+            });
+        });
+        const button = app.configureElement('button', ratingBox, {
+            className: 'rating-box-button',
+            type: 'submit',
+            textContent: 'submit',
+        })
     },
 
     configureElement: function(element, parent, attrs) {
@@ -40,6 +56,7 @@ const app = {
         };
         return elem;
     },
+
 };
 
 document.addEventListener('DOMContentLoaded', app.init);
